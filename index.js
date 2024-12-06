1ddf108d2296481000a60c871d67f3d979b14aa9
Nanda <138947445+Nandaxy@users.noreply.github.com>
2024-12-06 20:29:23 +0700

    .

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/notes", noteRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
