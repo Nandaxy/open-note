320dbc9888425154682a9c4be595c1d2390f04db
Nanda <138947445+Nandaxy@users.noreply.github.com>
2024-12-06 19:58:35 +0700

    Coba lagi

--- index.js
+++ index.js
@@ -5,27 +5,30 @@ import path from "path";
 import { fileURLToPath } from "url";
 import dotenv from "dotenv";
 import noteRoutes from "./src/routes/noteRoutes.js";
+
 dotenv.config();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/notes", noteRoutes);
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
+// Start server
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
