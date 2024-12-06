4f39510ee7f36919fa1f235e396b1c0956bca490
Nanda <138947445+Nandaxy@users.noreply.github.com>
2024-12-06 19:47:47 +0700

    Fix static file

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/api/notes", noteRoutes);
 
 mongoose
