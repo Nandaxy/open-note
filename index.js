441d8ee8a71368698e7d726cc7029ba60cc94f94
Nanda <138947445+Nandaxy@users.noreply.github.com>
2025-03-05 19:00:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,10 +24,17 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("MongoDB connection error:", err));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/index.html"));
+//app.get("/", (req, res) => {
+//  res.sendFile(path.join(__dirname, "./public/index.html"));
+//});
+
+app.get("/maintenance", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/bug.html"));
 });
 
+app.get("/", (req, res) => {
+  res.redirect("/maintenance");
+});
 // Start server
 const PORT = 3000;
 app.listen(PORT, () => {
